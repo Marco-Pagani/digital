3ed6e7e3b10c74a43f3e90fb79ab7d1d5cc3a274
Marco Pagani <marco@pagani.io>
2023-02-07 21:47:02 -0500

    stacking cards

--- src/components/card/card-base.vue
+++ src/components/card/card-base.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="rounded-lg w-[450px] p-6" :class="`${bgColors[color]}`">
+  <div class="rounded-lg w-[450px] p-6 pt-4 shadow-md" :class="`${bgColors[color]}`">
     <header class="flex justify-between">
       <h2 class="text-xl">{{ title }}</h2>
       <p>{{ subtitle }}</p>
     </header>
-    <ul class="font-handwritten text-xl">
+    <ul class="font-handwritten text-xl" v-if="showList">
       <li v-for="index in 10" :key="index" class="flex w-full gap-6 p-3 rounded-lg mt-3"
         :class="`focus-within:${focusColors[color]}`">
         <span>0</span>
@@ -19,7 +19,8 @@
 defineProps({
   title: String,
   subtitle: String,
-  color: Number
+  color: Number,
+  showList: Boolean
 })
 const bgColors = ['bg-light-brown', 'bg-medium-brown', 'bg-dark-brown']
 const focusColors = ['bg-medium-brown', 'bg-dark-brown', 'bg-light-brown']
--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -1,13 +1,15 @@
 <template>
   <main class="p-8 gap-6 bg-olive h-full">
-    <section class="flex">
-      <card-base title="Now" subtitle="2/7/23" color="0" class="z-30" />
-      <div class="bg-medium-brown rounded-lg font-printed w-[450px] -ml-[400px] -mb-3 mt-3 p-2 relative z-20">
+    <section class="flex pt-[100px]">
+      <card-base title="Now" subtitle="2/7/23" :color="0" :show-list="true" class="z-30" />
+      <card-base title="Later" subtitle="February" :color="1" class="z-20 -ml-[420px] mb-[50px] -mt-[50px]" />
+      <card-base title="Eventually" subtitle="My Dreams" :color="2" class="z-10 -ml-[420px] mb-[100px] -mt-[100px]" />
+      <!-- <div class="bg-medium-brown rounded-lg font-printed w-[450px] -ml-[400px] mb-[50px] -mt-[50px] p-2 relative z-20">
         <p class="text-xl absolute right-2 top-4 origin-top-left rotate-90 translate-x-full ">Later</p>
       </div>
       <div class="bg-dark-brown rounded-lg font-printed w-[450px] -ml-[400px] -mb-6 mt-6 p-2 relative">
         <p class="text-xl absolute right-2 top-4 origin-top-left rotate-90 translate-x-full ">Eventually</p>
-      </div>
+      </div> -->
     </section>
     <!-- <card-base title="Later" subtitle="February" color="1" />
     <card-base title="Eventually" subtitle="My Dreams" color="2" /> -->
