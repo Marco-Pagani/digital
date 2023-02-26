92739d35cd10cb8a3cfacf1d97927e3ffc2938b5
Marco Pagani <marco@pagani.io>
2023-02-26 00:30:05 -0500

    more PWA changes

--- vite.config.js
+++ vite.config.js
@@ -10,25 +10,41 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      // includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      // manifest: {
-      //   name: 'EMU: A digital task tracker',
-      //   short_name: 'EMU',
-      //   description: 'A digital take on an analog task system',
-      //   theme_color: '#aaaf8f',
-      //   icons: [
-      //     {
-      //       src: 'pwa-192x192.png',
-      //       sizes: '192x192',
-      //       type: 'image/png'
-      //     },
-      //     {
-      //       src: 'pwa-512x512.png',
-      //       sizes: '512x512',
-      //       type: 'image/png'
-      //     }
-      //   ]
-      // }
+      includeAssets: [
+        'android-chrome-512x512.png',
+        'android-chrome-192x192.png',
+        'apple-touch-icon.png'
+      ],
+      manifest: {
+        name: 'EMU: A digital task tracker',
+        short_name: 'EMU',
+        description: 'A digital take on an analog task system',
+        theme_color: '#aaaf8f',
+        lang: 'en',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
     })
   ],
   resolve: {
