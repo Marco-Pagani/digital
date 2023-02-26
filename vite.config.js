f8ec86f5f8c68b3da358266d5125a3735d6516e7
Marco Pagani <marco@pagani.io>
2023-02-26 00:20:33 -0500

    test PWA changes

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,31 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({
-    registerType: 'autoUpdate', devOptions: {
-      enabled: true
-    }
-  })],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      // manifest: {
+      //   name: 'EMU: A digital task tracker',
+      //   short_name: 'EMU',
+      //   description: 'A digital take on an analog task system',
+      //   theme_color: '#aaaf8f',
+      //   icons: [
+      //     {
+      //       src: 'pwa-192x192.png',
+      //       sizes: '192x192',
+      //       type: 'image/png'
+      //     },
+      //     {
+      //       src: 'pwa-512x512.png',
+      //       sizes: '512x512',
+      //       type: 'image/png'
+      //     }
+      //   ]
+      // }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
