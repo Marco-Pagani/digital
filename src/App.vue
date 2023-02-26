1c4298e809f46fc8daa2733859f15a18eb5eff9e
Marco Pagani <marco@pagani.io>
2023-02-25 23:02:00 -0500

    Layout fixes

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,15 @@ const version = APP_VERSION
 </script>
 
 <template>
-  <main class="bg-olive grow">
+  <main class="bg-olive grow pb-4 md:pb-0">
     <header class="bg-dark-brown text-center p-4">
-      <h1 class="font-printed text-3xl">Emulator</h1>
+      <h1 class="font-printed text-3xl">Emu</h1>
     </header>
     <TasksView></TasksView>
   </main>
-  <footer class="bg-dark-brown py-6 px-8 flex justify-between gap-6 text-sm">
+  <footer
+    class="bg-dark-brown py-6 px-8 flex justify-between gap-6 text-sm flex-wrap"
+  >
     <div class="flex items-center gap-2">
       <img src="/footer_logo.webp" class="w-7 -mb-2" />
       <p>
